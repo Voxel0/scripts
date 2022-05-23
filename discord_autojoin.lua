72a4285e5327ba5752ae4c553d4cc139b4e4788b
Voxel <80284601+Voxel0@users.noreply.github.com>
2022-05-23 16:46:47 -0500

    Update discord_autojoin.lua

--- discord_autojoin.lua
+++ discord_autojoin.lua
@@ -2,12 +2,5 @@ local Join = loadstring(game:HttpGet"https://raw.githubusercontent.com/Voxel0/sc
 local Code = game:HttpGet("https://raw.githubusercontent.com/Voxel0/scripts/main/discordcode")
 local GUI = game:GetService("StarterGui")
 local cb = Instance.new("BindableFunction")
-
-        coroutine.wrap(function() Join(Code) end)()
-        pcall(function() setclipboard("https://discord.gg/" .. Code) end)
-    	GUI:SetCore("SendNotification", {
-            Title = "Discord",
-            Text = "Attempted to join you into the server, we also set the invite to your clipboard!",
-            Duration = 5
-        })
-    
+coroutine.wrap(function() Join(Code) end)()
+pcall(function() setclipboard("https://discord.gg/" .. Code) end)
