337787db7de7aded4bd8600534cd9863e4936472
Voxel <80284601+Voxel0@users.noreply.github.com>
2022-10-14 17:31:07 -0500

    Create DHBP.lua

---
+++ DHBP.lua
@@ -0,0 +1,29 @@
+replaceclosure(isreadonly, function(t)
+  if t == syn then 
+    return true 
+  end 
+end)
+setreadonly(syn,false)
+local req = syn.request
+syn.request = function(args)
+  if args.Url == 'https://darkhub-v4.maxt.church/checkKey' then 
+    return {
+      Body = 'OK'
+    }
+  end 
+  return req(args)
+end 
+local isFile = isfile
+isFile = hookfunc(isfile,function(str)
+  if str:find('DarkHubKey.DARKHUB') then 
+    return true 
+  end 
+  return isFile(str)
+end)
+local readFile = readfile 
+readFile = hookfunc(readfile,function(str)
+  if str:find('DarkHubKey.DARKHUB') then 
+    return 'cheese'
+  end 
+  return readFile(str)
+end)
