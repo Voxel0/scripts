154ee4466327a26bc519fc7cf791e393b9ef5fdb
Voxel <80284601+Voxel0@users.noreply.github.com>
2022-10-30 15:53:35 -0700

    Update DHBP.lua

--- DHBP.lua
+++ DHBP.lua
@@ -6,24 +6,10 @@ end)
 setreadonly(syn,false)
 local req = syn.request
 syn.request = function(args)
-  if args.Url == 'https://api.darkhub.cloud/v3/roblox/checkKey' then 
+  if args.Url == 'https://buy.khysarth.ga/' then 
     return {
       Body = 'OK'
     }
   end 
   return req(args)
 end 
-local isFile = isfile
-isFile = hookfunc(isfile,function(str)
-  if str:find('DarkHubKey.DARKHUB') then 
-    return true 
-  end 
-  return isFile(str)
-end)
-local readFile = readfile 
-readFile = hookfunc(readfile,function(str)
-  if str:find('DarkHubKey.DARKHUB') then 
-    return 'cheese'
-  end 
-  return readFile(str)
-end)
